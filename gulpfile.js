21b64f3948d83e4329b8a01aec6fefbb65a1fdc1
andrewsosa001 <andrewsosa001@gmail.com>
2017-02-16 14:43:49 -0500

    Now with pug!

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
-var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var pug  = require('gulp-pug');
 
+// Compiles .pug files into .html
 gulp.task('pug', function buildHTML() {
   return gulp.src('src/*.pug')
-  .pipe(pug())
-	.pipe(gulp.dest('build')); // tell gulp our output folder
+	  .pipe(pug())
+		.pipe(gulp.dest('build')); // tell gulp our output folder
+});
+
+// Compiles .sass into .css
+gulp.task('sass', function() {
+	gulp.src('src/sass/*.sass')
+		.pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('build/css/'));
+});
+
+// Monitors .pug and .sass
+gulp.task('watch', function() {
+	gulp.watch('src/**', ['pug', 'sass']);
 });
 
 gulp.task('test', function() {
@@ -12,4 +26,4 @@ gulp.task('test', function() {
 	console.log("hello, world!")
 });
 
-gulp.task('default', ['pug']);
+gulp.task('default', ['watch']);
---
+++ upload.sh
@@ -0,0 +1,1 @@
+scp -r build/* andrewthewizard.com:/var/www/andrewsosa.com/html
