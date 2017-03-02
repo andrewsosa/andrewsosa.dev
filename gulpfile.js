f5f9c1db39af571d01c6e3e06b4517d227fb1b6d
andrewsosa001 <andrewsosa001@gmail.com>
2017-03-02 14:58:26 -0500

    Update gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,76 @@
+var fs = require('fs');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var pug  = require('gulp-pug');
 
-var dest = 'html';
+var build = 'html';
+
+// Retrieve Github pins
+gulp.task('github', function() {
+    var eeyore = require('eeyore');
+    return repos = eeyore('andrewsosa001', {
+
+    }).then(repos => {
+        var data = JSON.stringify({repos: repos});
+        fs.writeFile('src/github.json', data);
+    });
+
+});
 
 // Compiles .pug files into .html
-gulp.task('pug', function() {
-  return gulp.src('src/*.pug')
-	  .pipe(pug())
-		.pipe(gulp.dest(dest)); // tell gulp our output folder
+gulp.task('pug', ['github'], function() {
+    var pug = require('gulp-pug');
+    var data = require('gulp-data');
+    var locals = require('./src/locals.js');
+    gulp.src('src/pug/*.pug')
+        .pipe(data(files => {
+            return require('./src/github.json');
+        }))
+        .pipe(pug({
+            locals: locals
+        }).on('error', console.log))
+        .pipe(gulp.dest(build)); // tell gulp our output folder
 });
 
 // Compiles .sass into .css
 gulp.task('sass', function() {
-	gulp.src('src/sass/*.sass')
-		.pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(dest + '/css/'));
+    var sass = require('gulp-sass');
+    gulp.src('src/sass/*.sass')
+        .pipe(sass().on('error', sass.logError))
+    	.pipe(gulp.dest(build + '/css/'));
+});
+
+// Copies bootstrap files into build
+gulp.task('bootstrap', function() {
+    gulp.src('src/static/bootstrap/*.css')
+        .pipe(gulp.dest(build + '/css/'));
+    gulp.src('src/static/bootstrap/*.js')
+        .pipe(gulp.dest(build + '/js/'))
+});
+
+// Copies assets into build
+gulp.task('assets', function() {
+    gulp.src('src/static/assets/*')
+        .pipe(gulp.dest(build + '/assets/'))
+})
+
+// Removes built files
+gulp.task('clean', function() {
+    var clean = require('gulp-clean');
+    gulp.src('html/**', {read:false})
+        .pipe(clean());
 });
 
 // Monitors .pug and .sass
 gulp.task('watch', function() {
-	gulp.watch('src/**', ['pug', 'sass']);
+    gulp.watch('src/**', ['pug', 'sass']);
 });
 
-gulp.task('test', function() {
-  // place code for your default task here
-	console.log("hello, world!")
-});
+// Single time build of files
+gulp.task('build', ['pug', 'sass', 'bootstrap', 'assets']);
+gulp.task('default', ['build']);
 
-gulp.task('default', ['watch']);
+
+gulp.task('repos', () => {
+    repos.then(res => {
+        console.log(res)
+    })
+});
