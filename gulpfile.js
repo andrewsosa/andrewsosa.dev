5a3fd832e93b845beb127a47b5d69bfb6b7a3ad1
Unknown <andrewsosa001@gmail.com>
2017-06-28 01:25:56 -0400

    Update project depts and structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,76 @@
-var fs = require('fs');
-var gulp = require('gulp');
+const fs = require('fs');
+const gulp = require('gulp');
+const flatten = require('gulp-flatten')
+
 var locals = require('./src/locals.js');
 
 var build = 'html';
 var json = './src/json/'
 
-// Retrieve Github pins
+
+// Main Build tasks
+
+gulp.task('pug', ['github'], function() {
+    const pug    = require('gulp-pug');
+    const data   = require('gulp-data');
+    const locals = require('./src/locals.js');
+
+    gulp.src(['src/views/*/*.pug', '!src/views/_includes/**'])
+        .pipe(data(files => { return require(json + 'github.json'); }))
+        .pipe(pug({locals: locals}))
+        .on('error', console.log)
+        .pipe(flatten())
+        .pipe(gulp.dest(build));
+});
+
+gulp.task('sass', function() {
+    const sass = require('gulp-sass');
+    const paths = ['src/public/'];
+    gulp.src('src/views/*/*.sass')
+        .pipe(sass({includePaths: paths})
+            .on('error', sass.logError))
+        .pipe(flatten())
+        .pipe(gulp.dest(build + '/css'));
+});
+
+// Resource tasks
+
 gulp.task('github', function() {
     var eeyore = require('eeyore');
-    return repos = eeyore(locals.me, {
-
-    }).then(repos => {
+    return repos = eeyore(locals.me, {}).then(repos => {
         var data = JSON.stringify({repos: repos});
         fs.writeFile(json + 'github.json', data);
     });
 
 });
 
-// Compiles .pug files into .html
-gulp.task('pug', ['github'], function() {
-    var pug = require('gulp-pug');
-    var data = require('gulp-data');
-    gulp.src('src/pug/*.pug')
-        .pipe(data(files => {
-            return require(json + 'github.json');
-        }))
-        .pipe(pug({
-            locals: locals
-        }).on('error', console.log))
+gulp.task('assets', function() {
+    gulp.src('src/public/assets/*')
+        .pipe(gulp.dest(build + '/public/'))
+    gulp.src('src/public/favicon/*')
         .pipe(gulp.dest(build));
+
 });
 
-// Compiles .sass into .css
-gulp.task('sass', function() {
-    var sass = require('gulp-sass');
-    gulp.src('src/sass/*.sass')
-        .pipe(sass().on('error', sass.logError))
-    	.pipe(gulp.dest(build + '/css/'));
-});
 
-// Copies bootstrap files into build
-gulp.task('bootstrap', function() {
-    gulp.src('src/static/bootstrap/*.css')
-        .pipe(gulp.dest(build + '/css/'));
-    gulp.src('src/static/bootstrap/*.js')
-        .pipe(gulp.dest(build + '/js/'))
-});
+// Utility tasks
 
-// Copies assets into build
-gulp.task('assets', ['favicon'], function() {
-    gulp.src('src/static/assets/*')
-        .pipe(gulp.dest(build + '/assets/'))
-});
-
-// Copy favicons into place
-gulp.task('favicon', function() {
-    gulp.src('src/static/favicon/*')
-        .pipe(gulp.dest(build));
-});
-
-// Removes built files
 gulp.task('clean', function() {
     var clean = require('gulp-clean');
     gulp.src('html/**', {read:false})
         .pipe(clean());
 });
 
-// Monitors .pug and .sass
-gulp.task('watch', function() {
-    gulp.watch('src/**', ['pug', 'sass']);
+gulp.task('watch', ['build'], function() {
+    gulp.watch('src/views/**/*.pug', ['pug']);
+    gulp.watch('src/views/**/*.sass', ['sass']);
+    gulp.watch('src/public/**', ['assets']);
+    gulp.watch('src/locals.js', ['pug'])
 });
 
-// Single time build of files
-gulp.task('build', ['pug', 'sass', 'bootstrap', 'assets']);
+gulp.task('views', ['pug', 'sass']);
+gulp.task('build', ['views', 'assets']);
 gulp.task('default', ['build']);
-
 
 gulp.task('repos', () => {
     repos.then(res => {
