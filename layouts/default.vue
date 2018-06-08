2a62c94fad54d2402bc22a0d811ab1c5c4c3ea1e
Andrew Sosa <andrewsosa001@gmail.com>
2018-06-08 00:10:14 -0400

    Introduce nuxt.js

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="wrapper">
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="sass">
+  html, body, .wrapper
+    min-height: 100vh
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+
+  section
+    .hero.header
+      .container
+        //- img(src="~/assets/code-lines.svg")
+        .title Andrew <br> Sosa
+        .subtitle Software Engineer
+
+    .section.content
+      .container
+        .title About
+
+</template>
+
+<script>
+</script>
+
+<style lang="sass">
+
+@import "~assets/bulma"
+
+.header
+  padding-top: 10vh
+
+  .container
+    background-image: url("~/assets/code-lines.svg")
+    background-repeat: no-repeat
+    padding-top: 12vw
+
+    .title
+      font-size: $size-1 * 1.5
+
+      +tablet
+        font-size: $size-1 * 2.5
+
+      +desktop
+        font-size: $size-1 * 5
+
+    .subtitle
+      font-weight: 100
+      font-size: $size-1
+
+
+</style>
