6cdc1af402ec9e165b2426bdbd4e54f3beb7ae7a
Andrew Sosa <andrew@andrewsosa.com>
2019-09-13 16:13:47 -0400

    Remove unused navbar

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -7,29 +7,23 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+// import { StaticQuery, graphql } from "gatsby";
 
-import Navbar from "../Navbar";
 import Footer from "../Footer";
 
 import "../../styles/reset.css";
 import "tachyons/css/tachyons.css";
 import "../../styles/global.css";
-import styles from "./layout.module.css";
 
 const Layout = ({ children }) => (
   <>
-    {/* pa4-l */}
     <div className="pt2 pb4 normal mono primary-color">
       <div className="bg-white h-100 pa3 ph0-l pv6-l">
         <div className="center mw9 ph4">
-          {/* <Navbar /> */}
           {children}
           <Footer />
         </div>
       </div>
-
-      {/* <footer>© {new Date().getFullYear()} Andrew Sosa</footer> */}
     </div>
   </>
 );
