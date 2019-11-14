ddc72934bac76b5b6f3e344784d74eb07c93d434
Andrew Sosa <andrew@andrewsosa.com>
2019-11-13 22:59:05 -0500

    delete existing stuff, time to start over

--- src/components/layout/Section.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Section = ({ children }) => {
-  return <div className="mt5">{children}</div>;
-};
-
-export default Section;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,10 @@
 import React from "react";
-// import { Link } from "gatsby";
 
-import Layout, { Section } from "../components/layout";
 import SEO from "../components/seo";
-
-import Projects from "../components/content/Projects";
 
 const IndexPage = () => (
   <>
     <SEO title="Andrew Sosa" />
-    <Layout>
-      <Section>
-        <p className="lh-copy f3 f2-m f1-l tl">
-          Andrew Sosa is a software & dataops engineer, currently working at
-          L3Harris Technologies.
-        </p>
-      </Section>
-      <Projects limit={4} linkToPage />
-    </Layout>
   </>
 );
 
